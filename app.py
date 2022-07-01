32b0990e7b190bb44d583344decf37fec1118b03
PhanTung-06 <k.phantung@gmail.com>
2022-07-01 21:42:48 +0700

    update app.py with gdown model

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 import shutil
+import gdown 
 
 from utils.detector import Detector
 from detectron2.data.detection_utils import read_image
@@ -26,6 +27,10 @@ async def index(request: Request):
 async def create_upload_files(request: Request, image: UploadFile = File(...)):
     with open("./static/images/test.jpg", "wb") as buffer:
         shutil.copyfileobj(image.file, buffer)
+
+    url_model = "https://drive.google.com/file/d/15AI1jK6jQkxClQoClohajXW_CjtYBWYl/view?usp=sharing"
+    output = "trained_model.pth"
+    gdown.download(url_model, output)
 
     detector = Detector('configs/BAText/VinText/attn_R_50.yaml','trained_model.pth')
     img = read_image('./static/images/test.jpg', format='BGR')
